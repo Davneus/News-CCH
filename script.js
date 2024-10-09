136ae59cb9f81ecb30d2a1fd34d0400c7bf405e2
soportesistemas16 <soporte.sistemas.cch1@gmail.com>
2024-10-09 12:49:39 -0500

    1
    Primer diseño

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('abrirFormulario').addEventListener('click', function() {
+    document.getElementById('formulario').style.display = 'block';
+});
+
+document.getElementById('cerrarFormulario').addEventListener('click', function() {
+    document.getElementById('formulario').style.display = 'none';
+});
